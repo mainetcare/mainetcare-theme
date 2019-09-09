08f1ad8f325d4e56ddda7edd482cc74c19767dbf
Michael Mai <mm@mai-internet.de>
2019-09-09 11:26:59 +0200

    remove enqueuing of google fonts (used now in customizer)

--- functions.php
+++ functions.php
@@ -11,14 +11,14 @@ define( 'CHILD_THEME_MAINETCARE_THEME_VERSION', '1.0.0' );
 function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), CHILD_THEME_MAINETCARE_THEME_VERSION, 'all' );
 }
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 // Load Google Fonts:
-function custom_add_google_fonts() {
-	wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,900|Slabo+27px&display=swap', false );
-}
-add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
+//function custom_add_google_fonts() {
+//	wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,900|Slabo+27px&display=swap', false );
+//}
+//add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 add_filter( 'widget_text', 'do_shortcode' );
 
